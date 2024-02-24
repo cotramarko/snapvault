df2a20b3e4157935e43173923209582e5194558e
cotramarko <cotra.marko@gmail.com>
2024-02-24 19:24:57 +0100

    Add cobra files

---
+++ app/main.go
@@ -0,0 +1,15 @@
+/*
+Copyright © 2024 NAME HERE <EMAIL ADDRESS>
+*/
+package main
+
+import (
+	"fmt"
+
+	"github.com/cotramarko/snapvault/cmd"
+)
+
+func main() {
+	fmt.Println("Hello World!")
+	cmd.Execute()
+}
